9b3c541a734d6d8f68d329d3ecd45c6490b68f10
Ilya Golovchenko <ilya.golovchenko@jetbrains.com>
2021-11-18 15:35:27 +0300

    I-16 -- Make fallback parser following the general unpacking control config

--- parsers/parser_plain.go
+++ parsers/parser_plain.go
@@ -1,7 +1,16 @@
 package parsers
 
-import "github.com/2gis/loggo/common"
+import (
+	"github.com/2gis/loggo/common"
+	"github.com/2gis/loggo/configuration"
+)
 
-func ParseStringPlain(line []byte) common.EntryMap {
-	return common.EntryMap{LogKeyLog: string(line)}
+func CreateParserPlain(config configuration.ParserConfig) func(line []byte) common.EntryMap {
+	return func(line []byte) common.EntryMap {
+		result := common.EntryMap{}
+		baseMap := selectBaseMap(result, config.UserLogFieldsKey)
+		baseMap[config.RawLogFieldKey] = string(line)
+
+		return result
+	}
 }
--- parsers/parser_plain_test.go
+++ parsers/parser_plain_test.go
@@ -6,8 +6,25 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/2gis/loggo/common"
+	"github.com/2gis/loggo/configuration"
 )
 
 func TestParseLinePlain(t *testing.T) {
-	assert.Equal(t, common.EntryMap{"log": "test"}, ParseStringPlain([]byte("test")))
+	assert.Equal(t,
+		common.EntryMap{"log": common.EntryMap{"msg": "test"}},
+		CreateParserPlain(configuration.ParserConfig{
+			UserLogFieldsKey: "log",
+			RawLogFieldKey:   "msg",
+		},
+		)([]byte("test")),
+	)
+
+	assert.Equal(t,
+		common.EntryMap{"log": "test"},
+		CreateParserPlain(configuration.ParserConfig{
+			UserLogFieldsKey: "",
+			RawLogFieldKey:   "log",
+		},
+		)([]byte("test")),
+	)
 }
